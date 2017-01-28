453f3d750a9b594651ef12e55929379cd47560aa
Joan Ortega <jomaora@gmail.com>
2017-01-28 17:26:56 +0100

    adding passport middleware and post login

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const APIError = require('../lib/apiError');
+const passport = require('passport');
 
 router.get('/', (req, res) => {
     const err = (req.session.err) ? req.session.err : null;
@@ -9,5 +10,13 @@ router.get('/', (req, res) => {
     }
     next(new APIError(406, 'Not valid type for asked ressource'));
 });
+
+router.post('/',
+   passport.authenticate('local', {
+       successRedirect: '/songs',
+       failureRedirect: '/login',
+       failureFlash: true
+     })
+);
 
 module.exports = router;
